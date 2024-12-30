ba650c5b7e3e16ebcfdab5819ef1a98b887182d0
sudharsan319 <sudharsank319@gmail.com>
2024-12-30 16:02:45 +0530

    removed print joint_angles

--- src/notspot_controller/scripts/robot_controller_gazebo.py
+++ src/notspot_controller/scripts/robot_controller_gazebo.py
@@ -64,7 +64,7 @@ while not rospy.is_shutdown():
     try:
         joint_angles = inverseKinematics.inverse_kinematics(leg_positions,
                                dx, dy, dz, roll, pitch, yaw)
-        print(joint_angles)
+        # print(joint_angles)
         for i in range(len(joint_angles)):
             
             publishers[i].publish(joint_angles[i])
